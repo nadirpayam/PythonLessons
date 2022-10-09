ad1eabfe40f9929371b817e27ff2783f2f1513c3
Nadir Payam <nadirpayam@hotmail.com>
2022-10-09 15:58:47 +0300

    Lesson3 If-Else Claueses Added

---
+++ Lesson3_/1_if-else.py
@@ -0,0 +1,6 @@
+#if-else syntaxı
+if 3>8:
+    if 3>0:
+      print('big')
+else:
+    print("small")
---
+++ Lesson3_/2_if-elif.py
@@ -0,0 +1,11 @@
+# if elif mantığı javadaki else if yani
+
+x = 20
+y = 20
+
+if x >y:
+    print("x y'den büyük")
+elif x==y:
+    print('x ve y eşit')
+else:
+    print('x yden küçük')
